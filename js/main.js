731c86f6da8fa7747638227417429e28ec29be85
MichaelLebwoski <michael.schrupp@gmx.de>
2016-08-14 14:00:45 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,83 +1,90 @@
-var el,
-    width;
-
 $(window).load(function () {
-    el = $('nav .navigation-list li:first-child');
-    width = el.outerWidth();
-    $('.vertical-animation').css('width', width);
-    navAnim();
-});
-
-$(function () {
-    mobileView();
-});
-
-function navAnim() {
-    var index,
-        height,
-        id,
-        element,
-        leftPosition,
-        activeId,
-        activeElement,
-        activePosition,
-        activeWith;
-    $('.navigation-list ul li')
-        .mouseover(function () {
-            index = $(this).index();
-            width = $(this).width();
-            activeId = $('.navigation-list ul li.active').attr('id');
-            activeElement = document.getElementById(activeId);
-            activePosition = activeElement.offsetLeft;
-            activeWith = $('.navigation-list ul li.active').width();
-            id = $(this).attr('id');
-            element = document.getElementById(id);
-            leftPosition = element.offsetLeft;
-            $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
-                'width': width,
-                'left': leftPosition
-            }, 'slow');
-            $(this).click(function () {
-                if (!$(this).hasClass('active')) {
-                    activeId = $(this).attr('id');
-                    activeElement = document.getElementById(activeId);
-                    activePosition = activeElement.offsetLeft;
-                    $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
-                        'width': width,
-                        'left': leftPosition
-                    }, 'slow');
-                    $('.navigation-list ul li').removeClass('active');
-                    $(this).addClass('active');
-                }
-            });
-        })
-        .mouseleave(function () {
-            if (!$(this).hasClass('active')) {
-                $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
-                    'width': activeWith,
-                    'left': activePosition,
-                }, 'slow');
-            }
-        });
-}
-
-function mobileView() {
-    var windowWidth,
+    $('.vertical-animation').css('width', $('nav .navigation-list li:first-child').outerWidth());
+    var windowWidth = $(window).width(),
         navWidth = $('.home').outerWidth() + $('.navigation-list').outerWidth();
-
-    function mobileStyle() {
-        windowWidth = $(window).width();
-        if (windowWidth < navWidth) {
-            $('#navigation').addClass('mobile');
-        } else {
-            $('#navigation').removeClass('mobile');
-        }
+    if (windowWidth >= navWidth) {
+        desktopView();
+    } else {
+        mobileView();
     }
-    mobileStyle();
     $(window).resize(function () {
-        mobileStyle();
+        windowWidth = $(window).width();
+        if (windowWidth >= navWidth) {
+            desktopView();
+        } else {
+            mobileView();
+        }
     });
 
+});
+
+function desktopView() {
+    $('#navigation').removeClass('mobile');
+    $('.navigation-list ul').css('left', 0);
+
+    function navAnim() {
+
+        var width,
+            index,
+            height,
+            id,
+            element,
+            leftPosition,
+            activeId,
+            activeElement,
+            activePosition,
+            activeWith;
+
+        width = $('.navigation-list ul li.active').width();
+        activeWith = $('.navigation-list ul li.active').width();
+        $('.navigation-list ul li.active').closest('.navigation-list').find('.vertical-animation').stop().animate({
+            'width': width
+        }, 0);
+
+        $('.navigation-list ul li')
+            .mouseover(function () {
+                index = $(this).index();
+                width = $(this).width();
+                activeId = $('.navigation-list ul li.active').attr('id');
+                activeElement = document.getElementById(activeId);
+                activePosition = activeElement.offsetLeft;
+                activeWith = $('.navigation-list ul li.active').width();
+                id = $(this).attr('id');
+                element = document.getElementById(id);
+                leftPosition = element.offsetLeft;
+                $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
+                    'width': width,
+                    'left': leftPosition
+                }, 'slow');
+                $(this).click(function () {
+                    if (!$(this).hasClass('active')) {
+                        activeId = $(this).attr('id');
+                        activeElement = document.getElementById(activeId);
+                        activePosition = activeElement.offsetLeft;
+                        $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
+                            'width': width,
+                            'left': leftPosition
+                        }, 'slow');
+                        $('.navigation-list ul li').removeClass('active');
+                        $(this).addClass('active');
+                    }
+                });
+            })
+            .mouseleave(function () {
+                if (!$(this).hasClass('active')) {
+                    $(this).closest('.navigation-list').find('.vertical-animation').stop().animate({
+                        'width': activeWith,
+                        'left': activePosition,
+                    }, 'slow');
+                }
+            });
+    }
+    navAnim();
+}
+
+
+function mobileView() {
+    $('#navigation').addClass('mobile');
     $('#mobile-nav').click(function () {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
@@ -85,16 +92,15 @@ function mobileView() {
                 'left': '100%'
             }, function () {
                 $(this).next('ul').removeClass('active');
+
             });
         } else {
             $(this).addClass('active');
             $(this).next('ul').stop().animate({
-                    'left': '0'
-                }, function () {
-                    $(this).next('ul').addClass('active');
-                }
-            }
-            $(this).next('ul').addClass('active');
+                'left': '0'
+            }, function () {
+                $(this).next('ul').addClass('active');
+            });
         }
     });
 }
